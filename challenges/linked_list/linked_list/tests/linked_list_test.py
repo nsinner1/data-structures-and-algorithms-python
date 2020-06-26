895809fbd51de6512c85bfbc18426f125c640994
natalie sinner <nataliesinner@hotmail.com>
2020-06-25 18:53:32 -0700

    refactored challenge 5

--- challenges/linked_list/linked_list/tests/linked_list_test.py
+++ challenges/linked_list/linked_list/tests/linked_list_test.py
@@ -1,10 +1,45 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import Node, LinkedList
 
-def test_LinkedList():
+
+def test_node_exist():
+    assert Node
+
+def test_LinkedList_exist():
     assert LinkedList
 
+def test_empty_list():
+    ll = LinkedList()
+    assert ll
 
-def test_append_val():
-    linkedlist = LinkedList()
-    linkedlist.insert(0)
-    assert str(linkedlist) == "{ 0 } -> { 1 } -> NULL"
+def test_insert_to_empty_list():
+    ll = LinkedList()
+    ll.insert('Sunday')
+    assert ll.head.value == 'Sunday'
+
+def test_insert_multiple_to_empty_list():
+    ll = LinkedList()
+    ll.insert('Tuesday')
+    ll.insert('Monday')
+    ll.insert('Sunday')
+    assert ll.head.value == 'Sunday'
+
+def test_next_node_value():
+    ll = LinkedList()
+    ll.insert('Tuesday')
+    ll.insert('Monday')
+    ll.insert('Sunday')
+    assert ll.head.next_node.value == 'Monday'
+
+def test_includes_value_true():
+    ll = LinkedList()
+    ll.includes('Monday')
+    assert True
+
+def test_includes_value_false():
+    ll = LinkedList()
+    actual = ll.includes('November')
+    expected = False
+    assert actual == expected
+
+
+
